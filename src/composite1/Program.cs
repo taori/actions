b48f8c9e9c2dbe038d0084d601bcbf6f2c5479f6
Andreas Müller <taori@users.noreply.github.com>
2024-08-03 03:48:41 +0200

    exclude from coverage

--- src/composite1/Program.cs
+++ src/composite1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly:ExcludeFromCodeCoverage]
 Console.WriteLine("Hello, World2!");
